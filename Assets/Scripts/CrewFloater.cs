7ffeb62d0722983cdc5f68f3949a26c67e20f9ed
cyj0711 <ddk0711@gmail.com>
2023-03-16 14:30:21 +0900

    Project Upload

---
+++ Assets/Scripts/CrewFloater.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewFloater : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private List<Sprite> sprites;
+
+    private bool[] crewStates = new bool[12];
+    private float timer = 0.5f;
+    private float distance = 11f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            SPawnFloatingCrew((EPlayerColor)Random.Range(0, 12), distance);
+            timer = 1f;
+        }
+    }
+
+    public void SPawnFloatingCrew(EPlayerColor playerColor, float dist)
+    {
+        if (!crewStates[(int)playerColor])
+        {
+            crewStates[(int)playerColor] = true;
+
+            float angle = Random.Range(0f, 360f);
+            Vector3 spawnPos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f) * dist;
+            Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+            float floatingSpeed = Random.Range(1f, 4f);
+            float rotateSpeed = Random.Range(-3f, 3f);
+
+            var crew = Instantiate(prefab, spawnPos, Quaternion.identity).GetComponent<FloatingCrew>();
+            crew.SetFloatingCrew(sprites[Random.Range(0, sprites.Count)], playerColor, direction, floatingSpeed, rotateSpeed, Random.Range(0.5f, 1f));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var crew = collision.GetComponent<FloatingCrew>();
+        if(crew!=null)
+        {
+            crewStates[(int)crew.playerColor] = false;
+            Destroy(crew.gameObject);
+        }
+    }
+}
